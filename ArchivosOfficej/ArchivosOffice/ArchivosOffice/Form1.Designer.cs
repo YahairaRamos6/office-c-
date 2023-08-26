aaf8be523b7ff97f16bd20c50a6154503ab5d701
Yahaira Guadalupe Ontiveros Ramos <113449034+YahairaRamos6@users.noreply.github.com>
2023-08-25 22:34:46 -0600

    Add files via upload

---
+++ ArchivosOfficej/ArchivosOffice/ArchivosOffice/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace ArchivosOffice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Ptbword = new System.Windows.Forms.PictureBox();
+            this.PtbExcel = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Ptbword)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PtbExcel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 92);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 29);
+            this.label1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(35, 126);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(312, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Navy;
+            this.panel1.ForeColor = System.Drawing.Color.Navy;
+            this.panel1.Location = new System.Drawing.Point(-1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(455, 26);
+            this.panel1.TabIndex = 6;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(124, 41);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(197, 31);
+            this.lblTitulo.TabIndex = 5;
+            this.lblTitulo.Text = "Archivos Office";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(221, 31);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Texto a guardar:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(37, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(191, 31);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Guardar como:";
+            // 
+            // Ptbword
+            // 
+            this.Ptbword.Image = ((System.Drawing.Image)(resources.GetObject("Ptbword.Image")));
+            this.Ptbword.Location = new System.Drawing.Point(43, 225);
+            this.Ptbword.Name = "Ptbword";
+            this.Ptbword.Size = new System.Drawing.Size(58, 50);
+            this.Ptbword.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ptbword.TabIndex = 9;
+            this.Ptbword.TabStop = false;
+            this.Ptbword.Click += new System.EventHandler(this.Ptbword_Click);
+            // 
+            // PtbExcel
+            // 
+            this.PtbExcel.Image = ((System.Drawing.Image)(resources.GetObject("PtbExcel.Image")));
+            this.PtbExcel.Location = new System.Drawing.Point(143, 225);
+            this.PtbExcel.Name = "PtbExcel";
+            this.PtbExcel.Size = new System.Drawing.Size(58, 50);
+            this.PtbExcel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PtbExcel.TabIndex = 10;
+            this.PtbExcel.TabStop = false;
+            this.PtbExcel.Click += new System.EventHandler(this.PtbExcel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 306);
+            this.Controls.Add(this.PtbExcel);
+            this.Controls.Add(this.Ptbword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Ptbword)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PtbExcel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox Ptbword;
+        private System.Windows.Forms.PictureBox PtbExcel;
+    }
+}
+
---
+++ ArchivosOfficej/ArchivosOffice/ArchivosOffice/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Word;
+using Microsoft.Office.Interop.PowerPoint;
+using Microsoft.Office.Core;
+
+namespace ArchivosOffice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+     private void Ptbword_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sf = new SaveFileDialog();
+            if (sf.ShowDialog() == DialogResult.OK)
+            {
+                Object obj = System.Reflection.Missing.Value;
+                Microsoft.Office.Interop.Word.Application word = new Microsoft.Office.Interop.Word.Application();
+
+                Microsoft.Office.Interop.Word.Document doc = word.Documents.Add(ref obj);
+                doc.Activate();
+                word.Selection.TypeText(textBox1.Text);
+                doc.SaveAs2(sf.FileName);
+                word.Visible = true;
+            }
+        }
+
+        private void PtbExcel_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sf = new SaveFileDialog();
+            if (sf.ShowDialog() == DialogResult.OK)
+            {
+                Object obj = System.Reflection.Missing.Value;
+                Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+                Microsoft.Office.Interop.Excel.Workbook file = excel.Workbooks.Add(obj);
+                Microsoft.Office.Interop.Excel.Worksheet hoja = file.Worksheets.Add();
+                file.Activate();
+                hoja.Cells[1, 1].Value = textBox1.Text;
+                file.SaveAs(sf.FileName);
+                excel.Visible = true;
+            }
+
+        }
+    }
+    }
+
